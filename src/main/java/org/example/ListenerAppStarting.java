314f91d36df3bb94bb673fea69a74f843dadef0c
ndoom90 <ndoom90@gmail.com>
2021-05-13 11:36:55 +0900

    Create listener

---
+++ src/main/java/org/example/ListenerAppStarting.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+public class ListenerAppStarting implements ApplicationListener<ApplicationStartingEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+        System.out.println("=======================");
+        System.out.println("Application is starting");
+        System.out.println("=======================");
+    }
+
+}
+
+
